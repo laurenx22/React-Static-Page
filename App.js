d9f8f94d3e45bbf6170ff04cdce38a2fdeb82810
laurenx22 <la888446@ucf.edu>
2025-02-04 16:07:12 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import header from "./components/Header.js";
+import para1 from "./components/Content.js";
+import para2 from "./components/Content.js";
+import footer from "./components/Footer.js";
+
+
+console.log(header);
+
+console.log(para1);
+console.log(para2);
+
+console.log(footer);
---
+++ components/Content.js
@@ -0,0 +1,7 @@
+let para1 = ("Cinderalla is a Disney princess that has two evil step sister and an evil stepmother. She grows up having to clean everything while they sit and watch. She is very lonely and doesn't leave the house. She befriends the mice in the house.");
+
+
+let para2 = ("One day her Fairy Godmother appears. The fairy Godmother works her magic to get Cinderella to the  masked ball but she must be back before midnight. At the ball she dances with the Prince but then must leave. On her way out she looses her glass slipper. The price searched the whole kingdome to find her.");
+
+console.log(para1);
+console.log(para2);
